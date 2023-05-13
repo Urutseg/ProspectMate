528ddd7653e577b729008d947ee9bd9bd60d8dca
Urutseg <urutseg@gmail.com>
2023-05-13 13:36:21 -0700

    initial window implementation

--- ProspectMateGUI.lua
+++ ProspectMateGUI.lua
@@ -1,55 +1,73 @@
 local addonName, addonTable = ...
+local frame = CreateFrame("FRAME", "ProspectMateFrame")
 
+-- Create the main frame for the table
 local frame = CreateFrame("Frame", "ProspectMateFrame", UIParent, "UIPanelDialogTemplate")
 frame:SetSize(500, 400)
 frame:SetPoint("CENTER")
 frame:Hide()
 
+frame:SetMovable(true) -- Make the frame movable
+frame:EnableMouse(true) -- Allow the user to interact with the frame
+
+-- Create a title bar for the frame
+frame.title = frame:CreateFontString(nil, "OVERLAY", "GameFontNormal")
+frame.title:SetPoint("LEFT", frame.TitleBg, "LEFT", 5, 0)
+frame.title:SetText("ProspectMate")
+
+-- Set up the drag functionality for the frame
+frame:SetScript("OnMouseDown", function(self, button)
+    if button == "LeftButton" then
+        self:StartMoving()
+    end
+end)
+frame:SetScript("OnMouseUp", function(self, button)
+    if button == "LeftButton" then
+        self:StopMovingOrSizing()
+    end
+end)
+
+-- Create the scroll frame for the table
 local scrollFrame = CreateFrame("ScrollFrame", "ProspectMateScrollFrame", frame, "UIPanelScrollFrameTemplate")
 scrollFrame:SetSize(480, 320)
 scrollFrame:SetPoint("TOPLEFT", 10, -70)
 
+-- Create the child frame for the table
 local childFrame = CreateFrame("Frame", "ProspectMateChildFrame", scrollFrame)
 childFrame:SetSize(480, 320)
+
+-- Add the child frame to the scroll frame
 scrollFrame:SetScrollChild(childFrame)
 
+-- Create the header row for the table
 local headerRow = childFrame:CreateFontString(nil, "OVERLAY", "GameFontNormal")
 headerRow:SetPoint("TOPLEFT", 10, -10)
 headerRow:SetText("|cff00ccffOre Item|r   |cff00ccffProspecting Results|r")
 
-local function CreateProspectingRow(oreID, results, yOffset)
+-- Loop through the prospecting data and add rows to the table
+local yOffset = -30
+for oreID, results in pairs(SmartProspectorDB) do
     local oreName = GetItemInfo(oreID)
-    if not oreName then
-        return yOffset
-    end
-    
-    local row = childFrame:CreateFontString(nil, "OVERLAY", "GameFontHighlight")
-    row:SetPoint("TOPLEFT", 10, yOffset)
-    row:SetText(oreName)
+    if oreName then
+        local row = childFrame:CreateFontString(nil, "OVERLAY", "GameFontHighlight")
+        row:SetPoint("TOPLEFT", 10, yOffset)
+        row:SetText(oreName)
 
-    local prospectingResults = ""
-    for itemID, count in pairs(results) do
-        local itemName = GetItemInfo(itemID)
-        if itemName then
-            prospectingResults = prospectingResults .. itemName .. ": " .. count .. "\n"
+        local prospectingResults = ""
+        for itemID, count in pairs(results) do
+            local itemName = GetItemInfo(itemID)
+            if itemName then
+                prospectingResults = prospectingResults .. itemName .. ": " .. count .. "\n"
+            end
         end
-    end
 
-    local resultsRow = childFrame:CreateFontString(nil, "OVERLAY", "GameFontHighlight")
-    resultsRow:SetPoint("LEFT", row, "RIGHT", 10, 0)
-    resultsRow:SetText(prospectingResults)
+        local resultsRow = childFrame:CreateFontString(nil, "OVERLAY", "GameFontHighlight")
+        resultsRow:SetPoint("LEFT", row, "RIGHT", 10, 0)
+        resultsRow:SetText(prospectingResults)
 
-    return yOffset - 20
-end
-
-local function UpdateProspectingTable()
-    local yOffset = -30
-    for oreID, results in pairs(SmartProspectorDB) do
-        yOffset = CreateProspectingRow(oreID, results, yOffset)
+        yOffset = yOffset - 20
     end
 end
-
-UpdateProspectingTable()
 
 function addonTable:ToggleWindow()
     if frame:IsShown() then
@@ -62,12 +80,4 @@ end
 SLASH_PROSPECTMATE1 = "/prospectmate"
 SlashCmdList["PROSPECTMATE"] = function()
     addonTable:ToggleWindow()
-end
-
-function addonTable:SetInitialVisibility(visible)
-    if visible then
-        frame:Show()
-    else
-        frame:Hide()
-    end
 end
