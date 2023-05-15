f1413939a7fb536b5e1d6dcacaa1fa1ba3fbddbd
Urutseg <urutseg@gmail.com>
2023-05-15 11:07:12 -0700

    working table

--- ProspectMateGUI.lua
+++ ProspectMateGUI.lua
@@ -1,8 +1,56 @@
 local addonName, addonTable = ...
 
+-- Keep track of the created UI elements
+local uiElements = {}
+
+-- Function to clear existing UI elements
+local function ClearUIElements()
+    for _, element in ipairs(uiElements) do
+        element:SetText("") -- Clear the text of the font string
+        element:Hide() -- Hide the font string
+        element:SetParent(nil) -- Remove the element from its parent frame
+    end
+    uiElements = {} -- Clear the uiElements table
+end
+
+
+local function UpdateUIFrame(frame)
+    -- Clear existing UI elements
+    ClearUIElements()
+
+-- Loop through the prospecting data and add rows to the table
+    local yOffset = -30
+    for oreID, results in pairs(SmartProspectorDB) do
+        -- print(oreID)
+        local oreName = GetItemInfo(oreID)
+        if oreName then
+            local row = frame:CreateFontString(nil, "OVERLAY", "GameFontHighlight")
+            table.insert(uiElements, row) -- Add the font string to the list of UI elements
+
+            row:SetPoint("TOPLEFT", 10, yOffset)
+            row:SetText(oreName)
+
+            local prospectingResults = ""
+            for itemID, count in pairs(results) do
+                local itemName = GetItemInfo(itemID)
+                if itemName then
+                    prospectingResults = prospectingResults .. itemName .. ": " .. count .. "\n"
+                end
+            end
+
+            local resultsRow = frame:CreateFontString(nil, "OVERLAY", "GameFontHighlight")
+            table.insert(uiElements, resultsRow) -- Add the font string to the list of UI elements
+            resultsRow:SetPoint("LEFT", row, "RIGHT", 10, 0)
+            resultsRow:SetText(prospectingResults)
+
+            yOffset = yOffset - 20
+        end
+    end
+end
+
 -- Create the main frame for the table
 local frame = CreateFrame("Frame", "ProspectMateFrame", UIParent, "UIPanelDialogTemplate")
-frame:SetSize(500, 400)
+frame:SetSize(500, 430)
 frame:SetPoint("CENTER")
 frame:Hide()
 
@@ -27,6 +75,13 @@ frame:SetScript("OnMouseUp", function(self, button)
     end
 end)
 
+-- Create the refresh button
+local refreshButton = CreateFrame("Button", "ProspectMateRefreshButton", frame, "UIPanelButtonTemplate")
+refreshButton:SetSize(100, 25)
+refreshButton:SetPoint("BOTTOMRIGHT", -10, 10)
+refreshButton:SetText("Refresh")
+
+
 -- Create the scroll frame for the table
 local scrollFrame = CreateFrame("ScrollFrame", "ProspectMateScrollFrame", frame, "UIPanelScrollFrameTemplate")
 scrollFrame:SetSize(460, 320)
@@ -44,30 +99,12 @@ local headerRow = childFrame:CreateFontString(nil, "OVERLAY", "GameFontNormal")
 headerRow:SetPoint("TOPLEFT", 10, -10)
 headerRow:SetText("|cff00ccffOre Item|r   |cff00ccffProspecting Results|r")
 
--- Loop through the prospecting data and add rows to the table
-local yOffset = -30
-for oreID, results in pairs(SmartProspectorDB) do
-    local oreName = GetItemInfo(oreID)
-    if oreName then
-        local row = childFrame:CreateFontString(nil, "OVERLAY", "GameFontHighlight")
-        row:SetPoint("TOPLEFT", 10, yOffset)
-        row:SetText(oreName)
+UpdateUIFrame(childFrame)
 
-        local prospectingResults = ""
-        for itemID, count in pairs(results) do
-            local itemName = GetItemInfo(itemID)
-            if itemName then
-                prospectingResults = prospectingResults .. itemName .. ": " .. count .. "\n"
-            end
-        end
-
-        local resultsRow = childFrame:CreateFontString(nil, "OVERLAY", "GameFontHighlight")
-        resultsRow:SetPoint("LEFT", row, "RIGHT", 10, 0)
-        resultsRow:SetText(prospectingResults)
-
-        yOffset = yOffset - 20
-    end
-end
+-- Set up the button click handler
+refreshButton:SetScript("OnClick", function()
+    UpdateUIFrame(childFrame) -- Call the function to update the UI
+end)
 
 function addonTable:ToggleWindow()
     if frame:IsShown() then
